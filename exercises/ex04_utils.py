1f5868062f0d3962dcd483b91d43036d482cb42c
Donya Farahani <dfarah@unc.edu>
2023-10-09 16:01:48 -0400

    new update ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,6 +3,7 @@ author: str = "730661618"
 
 
 def all(num_list: list[int], num: int) -> bool:
+    """This function finds if all the elements of a list are the same value as an integer."""
     value: int = 0
     i: int = 0
 
@@ -19,20 +20,24 @@ def all(num_list: list[int], num: int) -> bool:
         if value == len(num_list):
             return True
 
+
 def max(num_list: list[int]) -> int:
+    """This function finds the maximum value in a given list."""
     if len(num_list) == 0:
         raise ValueError("max() arg is an empty List")
     
     i: int = 0
     max: int = num_list[0]
 
-    while i <= (len(num_list)-1):
+    while i <= (len(num_list) - 1):
         if num_list[i] > max:
             max = num_list[i]
         i += 1
     return max
 
+
 def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """This function tests if list1 and list2 are equal with the same elements in the same order."""
     if list1 == list2:
         return True
     else:
